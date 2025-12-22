ce731029c27e88d0a8b99494c07f7469e75b570f
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-12-22 17:28:15 +0300

    Add Ingredient model class

---
+++ src/main/java/Ingredient.java
@@ -0,0 +1,56 @@
+public class Ingredient {
+
+    private Integer id;
+    private String name;
+    private Double price;
+    private CategoryEnum category;
+    private Dish dish;
+
+    public String getDishName() {
+        if (dish == null) {
+            return null;
+        }
+        return dish.getName();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+}
+
